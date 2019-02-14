a5b4f6378100b47fa3d30aa020a728fbed41270d
Alan Pinhel de Souza <alan.pinhel@matera.com>
2019-02-14 14:27:37 -0200

    melhoria performance com style incorporado para elementos acima da quebra

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import './assets/css/reset.css';
-import './assets/css/main.css';
-import './assets/css/container.css';
-import './assets/css/cabecalho.css';
-import './assets/css/modalidades.css';
 import './assets/css/produtos.css';
 import './assets/css/periodos.css';
 import './assets/css/periodo.css';
