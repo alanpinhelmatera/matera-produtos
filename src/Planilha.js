62dbf382100cd7911994e6b44755826de4ac22c9
Alan Pinhel de Souza <alan.pinhel@matera.com>
2019-02-18 16:03:21 -0300

    implementação lógica para buscar dados na planilha

--- src/Planilha.js
+++ src/Planilha.js
@@ -1,24 +1,36 @@
-import dados from './assets/data/dados.json';
+import serializa from './Serializa';
+import mapeia from './Mapeia';
 
 export default (function planilha() {
+  const api = 'https://spreadsheets.google.com/feeds/list/';
+  const key = '1tliRVC8jhAiBL7RVvgYP-SkanrkNo0CPrgXTuEy7A-0';
+  const aba = { banco: 1, gestao: 2 };
+
   let dadosInterno = [];
 
   return {
     async getDados(modalidade) {
-      dadosInterno = dados[modalidade];
-      return dadosInterno;
+      dadosInterno = await fetch(`${api}${key}/${aba[modalidade]}/public/values?alt=json`)
+        .then(response => response.json())
+        .then(serializa)
+        .then(mapeia);
+      return { modalidade, ...dadosInterno };
     },
 
     async getPeriodos(idsProdutosSelecionados) {
       const periodos = dadosInterno.periodos
         .filter(periodo =>
           periodo.produtos
+            .filter(produto => produto.funcionalidades.length)
             .map(produto => produto.id)
             .some(idProduto => idsProdutosSelecionados.includes(idProduto))
         )
         .map(periodo => ({
           ...periodo,
-          produtos: periodo.produtos.filter(produto => idsProdutosSelecionados.includes(produto.id))
+          produtos: periodo.produtos.filter(
+            produto =>
+              idsProdutosSelecionados.includes(produto.id) && produto.funcionalidades.length
+          )
         }));
 
       return {
