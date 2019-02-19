334392d17ad243e35521f1e54d1425abf9950f12
Alan Pinhel de Souza <alan.pinhel@matera.com>
2019-02-19 09:47:35 -0300

    correção configuração de deploy

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/'
+    publicPath: '/matera-produtos'
   },
   module: {
     rules: [
